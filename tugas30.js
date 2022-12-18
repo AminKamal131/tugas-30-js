35e250a6f7a427b69a1df3b674426489d966307c
Amin Kamal <aminkamal355@gmail.com>
2022-12-18 21:54:48 +0800

    tugas 30
    tugas 30

---
+++ tugas30.js
@@ -0,0 +1,6 @@
+function panggil(spesifik){
+    var ambil = spesifik.match(/[es]/g)
+    console.log(ambil)
+}
+
+panggil("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat")
